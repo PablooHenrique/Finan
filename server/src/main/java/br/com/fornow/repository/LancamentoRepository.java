3e6e7f586ec8b7ca3b86e2ab6fffa26537526aab
Pablo <dev11@hojeti.com.br>
2017-09-27 14:13:42 -0300

    Adicionado a configuração do hibarnate e finalizado o lancamento repository

--- server/src/main/java/br/com/fornow/repository/LancamentoRepository.java
+++ server/src/main/java/br/com/fornow/repository/LancamentoRepository.java
@@ -1,27 +1,52 @@
 package br.com.fornow.repository;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 import br.com.fornow.model.Lancamento;
 
 public class LancamentoRepository {
-
+	
 	public void salvarLancamento(Lancamento lancamento) {
-		// TODO Auto-generated method stub
+		SessionFactory sessionFactory = Repository.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
 		
+		Transaction transaction = session.beginTransaction();
+		session.persist(lancamento);
+		transaction.commit();
+		session.close();
 	}
 
-	public void listar() {
-		// TODO Auto-generated method stub
+	public List<Lancamento> listar() {
+		SessionFactory sessionFactory = Repository.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
 		
+		Query<Lancamento> createQuery = session.createQuery("Select a from Lancamento a", Lancamento.class);
+		List<Lancamento> resultList = createQuery.getResultList();
+		session.close();
+		return resultList;
 	}
 
 	public void atualizarLancamento(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		
+		SessionFactory sessionFactory = Repository.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.merge(lancamento);
+		transaction.commit();
+		session.close();
 	}
 
 	public void deletar(long id) {
-		// TODO Auto-generated method stub
-		
+		SessionFactory sessionFactory = Repository.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Lancamento find = session.find(Lancamento.class, id);
+		session.remove(find);
+		transaction.commit();
+		session.close();
 	}
-
 }
