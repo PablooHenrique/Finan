5c8c6bebf90cbecff61f0ca01b6921c78861030c
Unknown <pabloohenrique32@gmail.com>
2018-02-24 17:03:57 -0300

    Ajustando o projeto cliente para receber o template do creative tim

---
+++ client/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+
+@Component({
+    selector: 'dashboard-cmp',
+    moduleId: module.id,
+    templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit{
+    ngOnInit(){
+    }
+}
